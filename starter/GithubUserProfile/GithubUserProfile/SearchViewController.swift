4514831be432a88a13bf12ba5357a05507fdef1c
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2023-02-21 14:06:35 +0900

    Add files via upload

---
+++ starter/GithubUserProfile/GithubUserProfile/SearchViewController.swift
@@ -0,0 +1,143 @@
+//
+//  SearchViewController.swift
+//  GithubUserSearch
+//
+//  Created by joonwon lee on 2022/05/25.
+//
+
+import UIKit
+import Combine
+import Kingfisher
+
+class UserProfileViewController: UIViewController {
+    
+    // setupUI
+    // userprofile
+    // bind     -> user가 업데이트 되면 UI까지 업데이트
+    // search control    -> network
+    
+    let network = NetworkService(configuration: .default)
+    
+    @Published private(set) var user: UserProfile?
+    var subscriptions = Set<AnyCancellable>()
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bind()
+        embedSearchControl()
+    }
+    
+    private func setupUI() {
+        thumbnail.layer.cornerRadius = 80
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "seungchan2022"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func bind() {
+        $user
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] result in
+                self.update(user)
+            }.store(in: &subscriptions)
+    }
+    
+    private func update(_ user: UserProfile?) {
+        // user가 nil일 수도 있으므로 guard 사용
+        guard let user = user else {
+            self.nameLabel.text = "n/a"
+            self.loginLabel.text = "n/a"
+            self.followingLabel.text = ""
+            self.followerLabel.text = ""
+            self.thumbnail.image = nil
+            return
+        }
+        
+        self.nameLabel.text = user.name
+        self.loginLabel.text = user.login
+        self.followingLabel.text = "following: \(user.following)"
+        self.followerLabel.text = "follower: \(user.followers)"
+        
+        // image url
+        // url -> image
+//        let url = URL(string: "https://example.com/image.jpg")    -> 오픈소스 활용
+//        imageView.kf.setImage(with: url)
+        
+        self.thumbnail.kf.setImage(with: user.avatarUrl)
+    }
+}
+
+extension UserProfileViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension UserProfileViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        
+        // Resuorce
+        let base = "https://api.github.com/"
+        let path = "users/\(keyword)"
+        let params: [String: String] = [:]
+        let header: [String: String] = ["Content-Type": "application/json"]
+        
+        var urlComponets = URLComponents(string: base + path)!
+        let queryItems = params.map { (key: String, value: String) in
+            return URLQueryItem(name: key, value: value)
+        }
+        urlComponets.queryItems = queryItems
+        
+        var request = URLRequest(url: urlComponets.url!)
+        header.forEach { (key: String, value: String) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+     
+        // NetworkService
+        URLSession.shared
+            .dataTaskPublisher(for: request)
+            .tryMap { result -> Data in
+                guard let response = result.response as? HTTPURLResponse,
+                      (200..<300).contains(response.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+            .decode(type: UserProfile.self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .sink { comletion in
+                print("completion: \(comletion)")
+                switch comletion {
+                case .failure(let error):
+                    self.user = nil
+                case .finished: break
+                }
+            } receiveValue: { user in
+                self.user = user
+            }.store(in: &subscriptions)
+    }
+}
+
+
+
