53a31eb49c6d1243243d9e46495639fca45b9c6d
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2023-02-25 12:03:31 +0900

    Add files via upload

---
+++ SwiftUI/Ch2_SwiftUI-View-이해하기/starter/SwiftUI-Basic/SwiftUI-Basic/ButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  ButtonView.swift
+//  SwiftUI-Basic
+//
+//  Created by 승찬 on 2023/02/24.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Button {
+            print("button tapped")
+        } label: {
+            Text("Click Me")
+        }
+
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView()
+    }
+}
