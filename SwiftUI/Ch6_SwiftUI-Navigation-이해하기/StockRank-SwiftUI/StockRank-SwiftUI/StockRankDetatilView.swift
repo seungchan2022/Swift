efe0fc0213af7459bfe00d0b49145ff2c480eeea
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2023-03-10 16:33:09 +0900

    Add files via upload

---
+++ SwiftUI/Ch6_SwiftUI-Navigation-이해하기/StockRank-SwiftUI/StockRank-SwiftUI/StockRankDetatilView.swift
@@ -0,0 +1,34 @@
+//
+//  StockRankDetatilView.swift
+//  StockRank-SwiftUI
+//
+//  Created by 승찬 on 2023/03/10.
+//
+
+import SwiftUI
+
+struct StockRankDetatilView: View {
+    
+    @Binding var stock: StockModel
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            Image(stock.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 120)
+            Text(stock.name)
+                .font(.system(size: 40, weight: .bold))
+            Text("\(stock.price) 원")
+                .font(.system(size: 25, weight: .bold))
+                .foregroundColor(stock.diff > 0 ? .red : .blue)
+        }
+    }
+}
+
+struct StockRankDetatilView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankDetatilView(stock: .constant( StockModel.list[0]))
+            .preferredColorScheme(.dark)
+    }
+}
